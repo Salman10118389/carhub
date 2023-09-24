7ef59ca87d166d729ca4626522a736d30c5e576e
salman10118389 <salman.10118389@mahasiswa.unikom.ac.id>
2023-09-24 11:07:55 +0700

    client side application

---
+++ components/index.tsx
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import CardCar from './CardCar';
+import ShowMore from './ShowMore';
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CardCar,
+    ShowMore
+}
